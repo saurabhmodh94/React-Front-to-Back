d2c32d766f481ebfc9c64de179b239a0a11211db
Saurabh Modh <saurabhmodh94@gmail.com>
2018-12-21 20:45:00 +0530

    removed unnecessary code

--- contact-manager/src/App.js
+++ contact-manager/src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <h1>App Component</h1>
+        {/*
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
@@ -20,6 +22,7 @@ class App extends Component {
             Learn React
           </a>
         </header>
+         */}
       </div>
     );
   }
