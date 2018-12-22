21f58869961f48610eb4807b766008354c677ba3
Saurabh Modh <saurabhmodh94@gmail.com>
2018-12-22 16:01:30 +0530

    toggle showContactInfo

--- contact-manager/src/components/Contact/Contact.js
+++ contact-manager/src/components/Contact/Contact.js
@@ -3,29 +3,34 @@ import React, { Component } from 'react';
 export class Contact extends Component {
   constructor() {
     super();
-    this.state = {};
+    this.state = {
+      showContactInfo: true
+    };
   }
   // onShowClick(name, e) {
   //   console.log(e, this.state, name);
   // }
   onShowClick = (name, e) => {
-    console.log(e, this.state, name);
+    // console.log(e, this.state, name);
+    this.setState({ showContactInfo: !this.state.showContactInfo });
   };
   render() {
     let { name, age, email } = this.props.contact;
     return (
       <div className="card card-body mb-3">
-        <ul className="list-group">
-          <li className="list-group-item">
-            Name: {name}
-            <i
-              onClick={this.onShowClick.bind(this, name)}
-              className="fas fa-sort-down"
-            />
-          </li>
-          <li className="list-group-item"> Age: {age}</li>
-          <li className="list-group-item"> Email: {email}</li>
-        </ul>
+        <h4 className="list-group-item">
+          {name}
+          <i
+            onClick={this.onShowClick.bind(this, name)}
+            className="fas fa-sort-down"
+          />
+        </h4>
+        {this.state.showContactInfo ? (
+          <ul className="list-group">
+            <li className="list-group-item"> Age: {age}</li>
+            <li className="list-group-item"> Email: {email}</li>
+          </ul>
+        ) : null}
       </div>
     );
   }
