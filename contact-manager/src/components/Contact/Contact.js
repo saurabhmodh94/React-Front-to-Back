3e2d088a5a8d377243b54aa91866c1ad20d5b388
Saurabh Modh <saurabhmodh94@gmail.com>
2018-12-22 15:36:41 +0530

    bind / arrow function

--- contact-manager/src/components/Contact/Contact.js
+++ contact-manager/src/components/Contact/Contact.js
@@ -1,13 +1,27 @@
 import React, { Component } from 'react';
 
 export class Contact extends Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
+  // onShowClick(name, e) {
+  //   console.log(e, this.state, name);
+  // }
+  onShowClick = (name, e) => {
+    console.log(e, this.state, name);
+  };
   render() {
     let { name, age, email } = this.props.contact;
     return (
       <div className="card card-body mb-3">
         <ul className="list-group">
           <li className="list-group-item">
-            Name: {name} <i class="fas fa-sort-down" />
+            Name: {name}
+            <i
+              onClick={this.onShowClick.bind(this, name)}
+              className="fas fa-sort-down"
+            />
           </li>
           <li className="list-group-item"> Age: {age}</li>
           <li className="list-group-item"> Email: {email}</li>
