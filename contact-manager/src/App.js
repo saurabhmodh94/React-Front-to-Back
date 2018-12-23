51c76b4ca18a8066afbd3e41b4446b62fd5b7d49
Saurabh Modh <saurabhmodh94@gmail.com>
2018-12-23 12:00:01 +0530

    AddContact Component

--- contact-manager/src/App.js
+++ contact-manager/src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import Contacts from './components/Contacts/Contacts';
 import Header from './components/Header/Header';
+import AddContact from './components/AddContact/AddContact';
+
 import { Provider } from './context';
 // import logo from './logo.svg';
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -16,6 +18,7 @@ class App extends Component {
         <div className="App">
           <Header branding="Contact Manager" />
           <div className="container">
+            <AddContact />
             <Contacts />
           </div>
           {/*
