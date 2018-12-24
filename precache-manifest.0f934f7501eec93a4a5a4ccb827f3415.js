08d8e5844188f745a3b919a476fa568a4c0c1a92
Saurabh Modh <saurabhmodh94@gmail.com>
2018-12-25 00:52:31 +0530

    Updates

---
+++ precache-manifest.0f934f7501eec93a4a5a4ccb827f3415.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "2b8fdbf7579a38197298",
+    "url": "/contact-manager/static/js/runtime~main.2b8fdbf7.js"
+  },
+  {
+    "revision": "d52d31a519f36cbe11eb",
+    "url": "/contact-manager/static/js/main.d52d31a5.chunk.js"
+  },
+  {
+    "revision": "d1fddad455774fc0306a",
+    "url": "/contact-manager/static/js/1.d1fddad4.chunk.js"
+  },
+  {
+    "revision": "d52d31a519f36cbe11eb",
+    "url": "/contact-manager/static/css/main.6311395e.chunk.css"
+  },
+  {
+    "revision": "d1fddad455774fc0306a",
+    "url": "/contact-manager/static/css/1.297565d3.chunk.css"
+  },
+  {
+    "revision": "bc278a15472d8c98d84ac6b21faf98d4",
+    "url": "/contact-manager/index.html"
+  }
+];
