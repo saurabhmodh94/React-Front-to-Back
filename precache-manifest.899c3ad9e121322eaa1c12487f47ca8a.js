27a450307e9b60d8d20e6e262e5d84b5b13399ea
Saurabh Modh <saurabhmodh94@gmail.com>
2019-01-01 12:38:21 +0530

    Updates

---
+++ precache-manifest.899c3ad9e121322eaa1c12487f47ca8a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "3057e6d3a36d0603186c",
+    "url": "/static/js/main.3057e6d3.chunk.js"
+  },
+  {
+    "revision": "7c04da3085e03ba3a675",
+    "url": "/static/js/1.7c04da30.chunk.js"
+  },
+  {
+    "revision": "3057e6d3a36d0603186c",
+    "url": "/static/css/main.9896d5ca.chunk.css"
+  },
+  {
+    "revision": "7c04da3085e03ba3a675",
+    "url": "/static/css/1.0bd771b4.chunk.css"
+  },
+  {
+    "revision": "96d0940ccc6fd548082eed1e60330077",
+    "url": "/index.html"
+  }
+];
