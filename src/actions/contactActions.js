31734f3787249e4d1b8465d493269b0e135bc645
Saurabh Modh <saurabhmodh94@gmail.com>
2019-01-01 10:22:17 +0530

    GET, DELETE, POST Contact Redux

--- src/actions/contactActions.js
+++ src/actions/contactActions.js
@@ -1,21 +1,35 @@
 import { GET_CONTACTS, DELETE_CONTACT, ADD_CONTACT } from './types';
-
-export const getContacts = () => {
-  return {
-    type: GET_CONTACTS
-  };
+import axios from 'axios';
+export const getContacts = () => async dispatch => {
+  const response = await axios.get(
+    'https://jsonplaceholder.typicode.com/users'
+  );
+  dispatch({
+    type: GET_CONTACTS,
+    payload: response.data
+  });
 };
 
-export const deleteContact = id => {
-  return {
-    type: DELETE_CONTACT,
-    payload: id
-  };
+export const deleteContact = id => async dispatch => {
+  try {
+    await axios.delete(`https://jsonplaceholder.typicode.com/users/${id}/`);
+  } catch (e) {
+    console.log(e);
+  } finally {
+    dispatch({
+      type: DELETE_CONTACT,
+      payload: id
+    });
+  }
 };
 
-export const addContact = contact => {
-  return {
+export const addContact = contact => async dispatch => {
+  const response = await axios.post(
+    `https://jsonplaceholder.typicode.com/users/`,
+    contact
+  );
+  dispatch({
     type: ADD_CONTACT,
-    payload: contact
-  };
+    payload: response.data
+  });
 };
--- src/reducers/contactReducer.js
+++ src/reducers/contactReducer.js
@@ -1,33 +1,15 @@
 import { GET_CONTACTS, DELETE_CONTACT, ADD_CONTACT } from '../actions/types';
 
 const initialState = {
-  contacts: [
-    {
-      id: 1,
-      name: 'Leanne Graham',
-      email: 'Sincere@april.biz',
-      phone: '1-770-736-8031 x56442'
-    },
-    {
-      id: 2,
-      name: 'Ervin Howell',
-      email: 'Shanna@melissa.tv',
-      phone: '010-692-6593 x09125'
-    },
-    {
-      id: 3,
-      name: 'Clementine Bauch',
-      email: 'Nathan@yesenia.net',
-      phone: '1-463-123-4447'
-    }
-  ]
+  contacts: []
 };
 
 export default function(state = initialState, action) {
   switch (action.type) {
     case GET_CONTACTS:
       return {
-        ...state
+        ...state,
+        contacts: action.payload
       };
     case DELETE_CONTACT:
       return {
