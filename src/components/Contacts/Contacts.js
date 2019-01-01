8d6acac7ce310e9ba60d1c7fffcead1fbc175034
Saurabh Modh <saurabhmodh94@gmail.com>
2019-01-01 08:21:52 +0530

    remove contect-api changes

--- src/components/Contacts/Contacts.js
+++ src/components/Contacts/Contacts.js
@@ -1,26 +1,30 @@
 import React, { Component } from 'react';
-import { Consumer } from './../../context';
+// import { Consumer } from './../../context';
 import Contact from './../Contact/Contact';
 
 export class Contacts extends Component {
+  state = {
+    contacts: []
+  };
+
   render() {
+    // return (
+    //   <Consumer>
+    //     {value => {
+    const { contacts } = this.state;
     return (
-      <Consumer>
-        {value => {
-          const contacts = value.contacts;
-          return (
-            <React.Fragment>
-              <h1 className="display-4 mb-2">
-                <span className="text-danger">Contact</span> List
-              </h1>
-              {contacts.map(contact => (
-                <Contact key={contact.id} contact={contact} />
-              ))}
-            </React.Fragment>
-          );
-        }}
-      </Consumer>
+      <React.Fragment>
+        <h1 className="display-4 mb-2">
+          <span className="text-danger">Contact</span> List
+        </h1>
+        {contacts.map(contact => (
+          <Contact key={contact.id} contact={contact} />
+        ))}
+      </React.Fragment>
     );
+    //     }}
+    //   </Consumer>
+    // );
   }
 }
 
