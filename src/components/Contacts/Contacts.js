57bf356ddf1d0dca211f1af95b5db2e07e9430d4
Saurabh Modh <saurabhmodh94@gmail.com>
2019-01-01 09:03:09 +0530

    connect(   mapStateToProps,   mapDispatchToProps )

--- src/components/Contacts/Contacts.js
+++ src/components/Contacts/Contacts.js
@@ -2,16 +2,20 @@ import React, { Component } from 'react';
 // import { Consumer } from './../../context';
 import Contact from './../Contact/Contact';
 
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { GET_CONTACTS } from '../../actions/types';
+
 export class Contacts extends Component {
-  state = {
-    contacts: []
-  };
+  componentDidMount() {
+    this.props.getContacts();
+  }
 
   render() {
     // return (
     //   <Consumer>
     //     {value => {
-    const { contacts } = this.state;
+    const { contacts } = this.props;
     return (
       <React.Fragment>
         <h1 className="display-4 mb-2">
@@ -28,4 +32,20 @@ export class Contacts extends Component {
   }
 }
 
-export default Contacts;
+Contacts.propTypes = {
+  contacts: PropTypes.array.isRequired,
+  getContacts: PropTypes.func.isRequired
+};
+
+const mapStateToProps = state => ({
+  contacts: state.contacts.contacts
+});
+
+const mapDispatchToProps = dispatch => ({
+  getContacts: () => dispatch({ type: GET_CONTACTS })
+});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Contacts);
--- src/reducers/contactReducer.js
+++ src/reducers/contactReducer.js
@@ -1,7 +1,15 @@
-const initialState = [];
+import { GET_CONTACTS } from '../actions/types';
+
+const initialState = {
+  contacts: []
+};
 
 export default function(state = initialState, action) {
   switch (action.type) {
+    case GET_CONTACTS:
+      return {
+        ...state
+      };
     default:
       return state;
   }
